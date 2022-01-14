57acee0e7ed2a473dede64c03c4889656cbbd053
isaac <exolivil@gamil.com>
2022-01-13 22:51:31 -0600

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Isaac on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
